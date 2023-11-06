e1078a42ef6ac7048e0fe0b9df14499de5db0d01
Pablo Swis <94465269+Pabloswis@users.noreply.github.com>
2023-11-06 16:57:49 -0300

    delay revenue and error in deleting invoices

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -20,7 +20,7 @@ export async function fetchRevenue() {
     // Don't do this in real life :)
 
     // console.log("Fetching revenue data...");
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -47,7 +47,7 @@ export async function fetchLatestInvoices() {
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
-    console.log(latestInvoices);
+    // console.log(latestInvoices);
     return latestInvoices;
   } catch (error) {
     console.error("Database Error:", error);
